66dfa508b8e815d57eb049243642ccb87364f6f0
danielemurgioni <dashomew@gmail.com>
2025-05-29 19:12:46 +0200

    [UPD] create route movie index

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+//importo della connessione
+const connection = require("./data/db")
+
 //rotta madre
 app.get("/", (req, res) => {
-    res.send("hello")
+    res.send("Movies entry point")
+})
+
+//rotta index
+app.get("/movies", (req, res) => {
+
+    const querySQL = "SELECT * FROM db_movie.movies"
+
+    connection.query(querySQL, (err, result) => {
+        if (err) { res.status(500).json({ error: "Database query failed" }) }
+        else { res.json(result) }
+    })
 })
 
 //server in ascolto per le richieste
